c3c1c08b8d74298608889a4c3f537cafdc3a55a9
Alveena Sohail <alveenasohail21@gmail.com>
2016-10-26 19:17:42 +0500

    safeUrlConvert added

--- app/src/common/common.module.js
+++ app/src/common/common.module.js
@@ -10,6 +10,38 @@
 
   angular.module('app.common', [
 
-  ]);
+      ])
+      .run(function($rootScope, FRONT_END_MEDIA_DEV_URL){
+
+        $rootScope.safeUrlConvert = safeUrlConvert;
+
+        // helper $rootScope methods
+
+        function safeUrlConvert(url){
+
+          // if default image
+          if(url.indexOf('svg/logo-icon.svg') >= 0){
+            return url;
+          }
+
+          // else
+
+          var isLocalhost = window.location.origin.indexOf('localhost');
+
+          // Development on Localhost (media serving through node.js)
+          if(isLocalhost){
+            var index = url.indexOf('/media');
+            var updatedUrl = FRONT_END_MEDIA_DEV_URL+url.substring(index)+'?t=1';
+            return updatedUrl;
+          }
+
+          // Production
+          else{
+            return url;
+          }
+
+        }
+
+      })
 
 }());
