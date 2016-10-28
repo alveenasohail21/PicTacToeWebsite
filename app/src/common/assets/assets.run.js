4a8429b315de178a48e05a706a73d34162b487c6
Alveena Sohail <alveenasohail21@gmail.com>
2016-10-29 01:22:24 +0500

    updates

--- app/src/common/assets/assets.run.js
+++ app/src/common/assets/assets.run.js
@@ -64,7 +64,7 @@
                      * */
                     loadFonts();
                     loadImagesAndSvgs();
-                    // stopDebug();
+                    stopDebug();
                 });
             }
 
