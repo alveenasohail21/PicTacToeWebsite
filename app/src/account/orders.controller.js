0f85289d6603ec9c2e97d5ab44ef3903b9ab261b
Alveena Sohail <alveenasohail21@gmail.com>
2016-11-01 14:10:23 +0500

    order confirm modal added

--- app/src/account/orders.controller.js
+++ app/src/account/orders.controller.js
@@ -13,15 +13,46 @@
         .controller('OrdersCtrl', OrdersCtrl);
 
     /* @ngInject */
-    function OrdersCtrl(r_orders, ordersFactory, $timeout){
+    function OrdersCtrl(r_orders, ordersFactory, $timeout, $rootScope){
         var vm = this;
         // vm.getOrders=getOrders;
         vm.orders = r_orders.orders;
+        vm.orderToCancel = null;
 
-        console.log(vm.orders);
+        vm.openConfirmCancelModal = openConfirmCancelModal;
+        vm.cancelOrder = cancelOrder;
 
         $timeout(function(){
             DataTable.init("#order-history");
         });
+
+        function openConfirmCancelModal(order){
+            vm.orderToCancel = order;
+            $('#confirmCancelOrderModal').modal({
+                keyboard: true
+            });
+        }
+
+        function cancelOrder(){
+
+            if(vm.orderToCancel){
+                ordersFactory.cancelOrder(vm.orderToCancel.id)
+                    .then(function(resp){
+                        if(resp.success){
+                            $rootScope.messageModal = {
+                                heading: 'Order has been cancelled',
+                                rightBtnText: 'Done',
+                                showLeftBtn: false
+                            };
+
+                            $('#messageModal').modal({
+                                keyboard: true
+                            })
+
+                        }
+                    })
+            }
+        }
+
     }
 }());
